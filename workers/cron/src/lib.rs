b614ab7eeaa228235b82c79558707403e576007a
pawurb <contact@pawelurbanek.com>
2024-10-18 11:28:24 +0200

    cron works

--- workers/cron/src/lib.rs
+++ workers/cron/src/lib.rs
@@ -5,11 +5,11 @@ use serde_json::json;
 use worker::*;
 use yt_sub_core::UserSettings;
 
-#[event(fetch)]
-async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-    wasm_rs_dbg::dbg!("Hello from the worker!");
-    wasm_rs_dbg::dbg!(req);
+#[event(scheduled)]
+async fn scheduled(evt: ScheduledEvent, env: Env, _ctx: ScheduleContext) {
     console_error_panic_hook::set_once();
+    wasm_rs_dbg::dbg!("Hello from the worker dbg!");
+    wasm_rs_dbg::dbg!(evt);
     let dbg_slack_webhook = env
         .var("DBG_SLACK_WEBHOOK")
         .expect("Missing DBG_SLACK_WEBHOOK env var");
@@ -19,15 +19,15 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         .await
         .unwrap();
 
-    let kv = env.kv("users")?;
+    let kv = env.kv("users").unwrap();
     let settings = kv
         .get("7ff01cf2-9ec5-4874-b885-a50c23f292eb")
         .text()
-        .await?;
-    let settings = settings.unwrap();
-
-    let settings: UserSettings = serde_json::from_str(&settings).unwrap();
-    Response::ok("Hello World!")
+        .await
+        .unwrap();
+    if let Some(settings) = settings {
+        let settings: UserSettings = serde_json::from_str(&settings).unwrap();
+    };
 }
 
 async fn dbg_slack_notify(message: &str, webhook_url: &str) -> EyreResult<()> {
