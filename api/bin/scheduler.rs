e488889e7137d1ed0d38d597f14f68e383886c12
pawurb <contact@pawelurbanek.com>
2024-11-12 14:25:21 +0100

    bin error handling

--- api/bin/scheduler.rs
+++ api/bin/scheduler.rs
@@ -1,14 +1,23 @@
 use eyre::Result;
 use std::time::Duration;
-use yt_sub_api::config::{middleware::init_logs, schedule::get_schedule};
+use yt_sub_api::config::{middleware, schedule::get_schedule};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    init_logs("scheduler.log");
+    match run().await {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            Err(e)
+        }
+    }
+}
+
+async fn run() -> Result<()> {
+    middleware::init_logs("scheduler.log");
 
     let sched = get_schedule().await?;
     sched.start().await?;
     tokio::time::sleep(Duration::MAX).await;
-
     Ok(())
 }
--- api/bin/server.rs
+++ api/bin/server.rs
@@ -1,7 +1,6 @@
-use std::net::TcpListener;
-
 use axum::middleware::from_fn;
 use eyre::Result;
+use std::net::TcpListener;
 use tracing::info;
 use yt_sub_api::{
     config::{middleware, routes::app},
@@ -10,12 +9,20 @@ use yt_sub_api::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    match run().await {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            Err(e)
+        }
+    }
+}
+
+async fn run() -> Result<()> {
     middleware::init_logs("server.log");
 
-    init_lite_db(None).await.expect("Failed to init sqlite db");
-    let conn = sqlite_conn(None)
-        .await
-        .expect("Failed to connect to sqlite db");
+    init_lite_db(None).await?;
+    let conn = sqlite_conn(None).await?;
     let app = app(conn)
         .await
         .layer(middleware::logging())
@@ -25,13 +32,10 @@ async fn main() -> Result<()> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
     if TcpListener::bind(format!("0.0.0.0:{port}")).is_err() {
-        tracing::error!("Port {} is already in use", port);
-        std::process::exit(1);
+        eyre::bail!("Port {} is already in use", port);
     }
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
 
     info!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
